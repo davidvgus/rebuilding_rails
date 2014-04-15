214ed831e6ef8ef8a11cd1f0c323d044e89f0c5a
David Gustafson <git@radicalskeptic.net>
2014-04-15 13:54:41 -0700

    Adds version number to sample output.

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,9 +3,11 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      `echo debug > debug.txt;`
+      binding.pry!
+
       [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers!"]]
+        ["Hello from Ruby on Rulers version: #{Rulers::VERSION}"]]
     end
   end
 end
